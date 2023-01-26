7b3f04ef14894008fc2e4577a8a863ee270c2dba
nelloreprt <nelloreprt@gmail.com>
2023-01-26 23:55:31 +0530

    create servers script

---
+++ 06-special-vars.sh
@@ -0,0 +1,22 @@
+echo "Below are the values from script"
+echo “0 is $0”
+echo “1 is $1”
+echo “2 is $2”
+echo “3 is $3”
+echo “* is $*”
+echo “@ is $@”
+echo “# is $#"
+
+example (){
+echo "Below are the values from function"
+echo “0 is $0”
+echo “1 is $1”
+echo “2 is $2”
+echo “3 is $3”
+echo “* is $*”
+echo “@ is $@”
+echo “# is $#"
+}
+
+#calling function "example"
+example
